5afb40f7de5d2afb4be82858e9b62e8456473921
Samaspp <92224079+Samaspp@users.noreply.github.com>
2022-11-23 15:06:42 +0530

    Add files via upload

--- st7.c
+++ st7.c
@@ -11,18 +11,6 @@ void push(char data)
 }
 char pop()
 {
-    char del;#include<stdio.h>
-#define SIZE 100
-char stack[SIZE],top=-1;
-void push(char data)
-{
-    if(top==SIZE-1)
-        printf("\nStack Overflow");
-    else   
-        stack[++top]=data;    
-}
-char pop()
-{
     char del;
     if(top==-1)
     {
@@ -35,13 +23,13 @@ char pop()
         return del;
     }
 }
-int isMatching(char character1,char character2)
+int isMatching(char c1,char c2)
 {
-    if(character1=='('&&character2==')')
+    if(c1=='('&&c2==')')
         return 1;
-    else if(character1=='{'&&character2=='}')
+    else if(c1=='{'&&c2=='}')
         return 1;
-    else if(character1=='['&&character2==']')
+    else if(c1=='['&&c2==']')
         return 1;
     else    
         return 0;            
@@ -88,7 +76,7 @@ int BracketsBalanced(char exp[])
    }
 int main()
 {
-    char exp[SIZE];
+    char exp[SIZE],del;
     printf("Enter an expression : ");
     scanf("%s",exp);
     int flag=BracketsBalanced(exp);
@@ -97,7 +85,7 @@ int main()
     else   
         printf("Not Balanced\n");    
     return 0;    
-}
+
     if(top==-1)
     {
         printf("\nStack empty.");
@@ -109,66 +97,4 @@ int main()
         return del;
     }
 }
-int isMatching(char character1,char character2)
-{
-    if(character1=='('&&character2==')')
-        return 1;
-    else if(character1=='{'&&character2=='}')
-        return 1;
-    else if(character1=='['&&character2==']')
-        return 1;
-    else    
-        return 0;            
-}
-int openingBracket(char ch)
-{
-    if(ch=='('||ch=='{'||ch=='[')
-        return 1;
-    else    
-        return 0;    
-}
-int closingBracket(char ch)
-{
-    if(ch==')'||ch=='}'||ch==']')
-        return 1;
-    else
-        return 0;    
-}
-int BracketsBalanced(char exp[])
-{
-    int i=0;
-    while(exp[i]!='\0')
-    {
-        if(openingBracket(exp[i]))
-            push(exp[i]);
-        else if(closingBracket(exp[i]))
-        {
-            if(top==-1)
-                return 0;
-            else
-            {
-                if(isMatching(pop(),exp[i]))
-                {
 
-                }
-                else
-                return 0;
-            }
-        }
-        i++;
-    }
-   if(top==-1)
-   return 1;
-   }
-int main()
-{
-    char exp[SIZE];
-    printf("Enter an expression : ");
-    scanf("%s",exp);
-    int flag=BracketsBalanced(exp);
-    if(flag)
-        printf("Balanced\n");
-    else   
-        printf("Not Balanced\n");    
-    return 0;    
-}
